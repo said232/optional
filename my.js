d3fac34a4cb640268258685eb068bbb87e273ec3
said232 <saidbilalocc@gmail.com>
2020-11-01 18:13:29 +0100

    first commit

---
+++ my.js
@@ -0,0 +1,10 @@
+function changeTitle(){
+    var title = document.querySelector("#mainTitle");
+    var para=document.querySelector("#paragraph");
+    title.style.color = 'black';
+    title.style.backgroundColor = "white";
+    title.style.border = "5px dashed aquarine";
+    title.innerHTML="Optional project"
+    para.innerHTML="<p> Welcome to the Opional project ,this is just the begining of javascript, have fun</p>";
+ 
+}
